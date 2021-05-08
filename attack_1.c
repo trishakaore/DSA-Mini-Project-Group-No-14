1557990fef24dc943e978ec086d0905b9fcaf788
trishakaore <71206575+trishakaore@users.noreply.github.com>
2021-05-08 23:01:08 +0530

    Update attack_1.c

--- attack_1.c
+++ attack_1.c
@@ -12,32 +12,37 @@ void attack()
 
     srand(time(NULL));
     int rndnum = ((rand()) % 50) + 1;
-
-    //t=t->next;
-    t = t->prev;
-
-    if (rndnum > t->block_index)
+    if (t != NULL)
     {
-        printf("Attack failed\n");
-        return;
-    }
 
-    while (t != NULL)
-    {
-        if (t->block_index == rndnum)
-        {
-            //prev_nonce = t->nonce;
-            changed = (rand() % 500) + 1;
-
-            while (changed == t->nonce)
-            {
-                changed = (rand() % 500) + 1;
-            }
-            printf("Attack Successful\n");
-
-            t->nonce = changed;
-            break;
-        }
+        //t=t->next;
         t = t->prev;
+
+        if (rndnum > t->block_index)
+        {
+            printf("Attack failed\n");
+            return;
+        }
+
+        while (t != NULL)
+        {
+            if (t->block_index == rndnum)
+            {
+                //prev_nonce = t->nonce;
+                changed = (rand() % 500) + 1;
+
+                while (changed == t->nonce)
+                {
+                    changed = (rand() % 500) + 1;
+                }
+                printf("Attack Successful\n");
+
+                t->nonce = changed;
+                break;
+            }
+            t = t->prev;
+        }
     }
+    else
+        printf("Attack failed - No block formed to attack!");
 }
