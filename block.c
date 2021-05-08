ee00b199204f96b1e7ec79f5a25d00cd3962f66b
trishakaore <71206575+trishakaore@users.noreply.github.com>
2021-05-08 23:35:00 +0530

    Update block.c

--- block.c
+++ block.c
@@ -5,18 +5,19 @@ block_ptr Init_block_chain()
 
     head = malloc(sizeof(block));
     head->prev = NULL;
+    head->next = NULL;
     head->block_index = 1;
-    strcpy(head->prev_block_hash, "0");
+    strcpy (head->prev_block_hash, "0");
     head->nonce = (rand() % 500) + 1;
 
-    trav = malloc(sizeof(block));
+    //trav = malloc(sizeof(block));
     trav = head;
     transactions = 0;
 
-    return head; //why global head?
+    return head;  //why global head?
 }
 
-void add_block() //always validate before add?  //after transaction has been called, if transactions == 50, then add_block
+void add_block()    //always validate before add?  //after transaction has been called, if transactions == 50, then add_block
 {
     block_num++;
     transactions = 0; //check
@@ -25,26 +26,31 @@ void add_block() //always validate before add?  //after transaction has been cal
     char hash_value[30];
 
     block_ptr tmp = malloc(sizeof(block));
-    //printf("initial %p\n", &tmp);
+
     tmp->block_index = block_num;
     tmp->nonce = (rand() % 500) + 1;
+    trav->next = tmp;
+    tmp->next = NULL;
     //must calc. the hash value of trav and store it in hash_value[30]
+
+    //strcpy(tmp->prev_block_hash, trav->prev_block_hash);
 
     tmp->prev = trav;
     trav = tmp;
 
-    strcpy(trav->prev_block_hash, hash_value);
-    //printf("\n%s\n", hash_value); // hafta do this using hash
-    //printf("final %p\n", &tmp);
+
+    strcpy(trav->prev_block_hash, hash_value);   // hafta do this using hash
+
     //free(tmp);
 
-    //    trav->next = tmp;
-    //    //must calc. the hash value of trav and store it in hash_value[30]
-    //
-    //    trav = tmp;
-    //
-    //    trav->next = NULL;
-    //    trav->nonce = (rand() % 500) + 1;
-    //    trav->block_index = block_num;
-    //    strcpy(trav->prev_block_hash, hash_value);   // hafta do this using hash
+//    trav->next = tmp;
+//    //must calc. the hash value of trav and store it in hash_value[30]
+//
+//    trav = tmp;
+//
+//    trav->next = NULL;
+//    trav->nonce = (rand() % 500) + 1;
+//    trav->block_index = block_num;
+//    strcpy(trav->prev_block_hash, hash_value);   // hafta do this using hash
+
 }
